470b3020a10fbe10ee5e39e10a0251278828b295
Paula De Souza Biazotto <pde-souz@c1r10s1.42lisboa.com>
2023-04-20 10:11:49 +0100

    add ft_find_nl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: pde-souz <pde-souz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 10:23:54 by pde-souz          #+#    #+#             */
-/*   Updated: 2023/04/20 09:45:44 by pde-souz         ###   ########.fr       */
+/*   Updated: 2023/04/20 10:11:24 by pde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,18 @@ char	*ft_strjoin(char *s1, char *s2, int c)
 	ret = nstr;
 	if (nstr == 0)
 		return (NULL);
-	while (s1 != 0 && *s1 != 0)
+	while (s1 != 0 && s1[i] != 0)
 	{
-		*nstr = *s1;
-		nstr++;
-		s1++;
+		*nstr++ = s1[i++];
 	}
+	i = 0;
 	while (i < c)
 	{
-		*nstr = *s2;
-		nstr++;
-		s2++;
+		*nstr++ = *s2++;
 		i++;
 	}
 	*nstr = 0;
+	free(s1);
 	return (ret);
 }
 
